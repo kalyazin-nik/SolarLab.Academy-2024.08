715ddc5d10d125d5aa42e189d37e4310c5dcdfb4
Nikita <kalyazin.nik@yandex.ru>
2024-10-13 16:55:54 +0300

    Рефакторинг документации сервиса объявлений.

--- src/SolarLab.Academy.AppServices/Contexts/Adverts/Services/IAdvertService.cs
+++ src/SolarLab.Academy.AppServices/Contexts/Adverts/Services/IAdvertService.cs
@@ -10,13 +10,13 @@ public interface IAdvertService
     /// <summary>
     /// Создает объявление по модели запроса.
     /// </summary>
+    /// <remarks>
+    /// Будет выбрашено исключение <see cref="BadRequestException"/>, в случае, если, у передаваемой модели, идентификатор окажется со значением null или по умолчанию. <br />
+    /// Также будет выбрашено исключение <see cref="EntityNotFoundException"/>, в случае, если модель категории отстутствует в репозитории.
+    /// </remarks>
     /// <param name="dto">Модель запроса.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    /// <returns>Идентификатор созданного объявления.
-    /// 
-    /// <br/><br/>Будет выбрашено исключение <see cref="BadRequestException"/>, в случае, если, у передаваемой модели,
-    /// идентификатор окажется со значением null или по умолчанию. Также будет выбрашено исключение <see cref="EntityNotFoundException"/>, в случае, 
-    /// если модель категории отстутствует в репозитории.</returns>
+    /// <returns>Идентификатор созданного объявления.</returns>
     /// <exception cref="BadRequestException"></exception>
     /// <exception cref="EntityNotFoundException"></exception>
     Task<Guid> CreateAsync(AdvertCreateDto dto, CancellationToken cancellationToken);
@@ -24,12 +24,13 @@ public interface IAdvertService
     /// <summary>
     /// Выполняет получение объявлений по идентификатору категории.
     /// </summary>
+    /// <remarks>
+    /// Будет выбрашено исключение <see cref="BadRequestException"/>, в случае, если идентификатор окажется со значением null или по умолчанию.<br/>
+    /// Также будет выбрашено исключение <see cref="EntityNotFoundException"/>, в случае, если модель категории отстутствует в репозитории.
+    /// </remarks>
     /// <param name="categoryId">Идентификатор категории.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    /// <returns>Колеекция сокращенных моделей объявления.
-    /// 
-    /// <br/><br/>Будет выбрашено исключение <see cref="BadRequestException"/>, в случае, если идентификатор 
-    /// окажется со значением null или по умолчанию. Также будет выбрашено исключение <see cref="EntityNotFoundException"/>, в случае, если модель категории отстутствует в репозитории.</returns>
+    /// <returns>Колеекция сокращенных моделей объявления.</returns>
     /// <exception cref="BadRequestException"></exception>
     /// <exception cref="EntitiesNotFoundException"></exception>
     Task<IReadOnlyCollection<AdvertSmallDto>> GetByCategoryIdAsync(Guid? categoryId, CancellationToken cancellationToken);
@@ -37,12 +38,13 @@ public interface IAdvertService
     /// <summary>
     /// Получает объявление по идентификатору.
     /// </summary>
+    /// <remarks>
+    /// Будет выбрашено исключение <see cref="BadRequestException"/>, в случае, если идентификатор окажется со значением null или по умолчанию.<br/>
+    /// Также будет выбрашено исключение <see cref="EntityNotFoundException"/>, в случае, если модель объявления отстутствует в репозитории.
+    /// </remarks>
     /// <param name="id">Идентификатор.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    /// <returns>Модель объявления.
-    /// 
-    /// <br/><br/>Будет выбрашено исключение <see cref="BadRequestException"/>, в случае, если идентификатор окажется со значением null или по умолчанию. 
-    /// Также будет выбрашено исключение <see cref="EntityNotFoundException"/>, в случае, если модель объявления отстутствует в репозитории.</returns>
+    /// <returns>Модель объявления.</returns>
     /// <exception cref="BadRequestException"></exception>
     /// <exception cref="EntityNotFoundException"></exception>
     Task<AdvertDto> GetByIdAsync(Guid? id, CancellationToken cancellationToken);
@@ -50,12 +52,12 @@ public interface IAdvertService
     /// <summary>
     /// Выполняет получение объявлений по поисковому запросу.
     /// </summary>
+    /// <remarks>
+    /// Будет выбрашено исключение <see cref="EntitiesNotFoundException"/>, в случае, если полученная коллекция от репозитория будет иметь значение null или пусто.
+    /// </remarks>
     /// <param name="request">Тело запроса.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    /// <returns>Колеекция моделей объявлений в сокращенном виде.
-    /// 
-    /// <br/><br/>Будет выбрашено исключение <see cref="EntitiesNotFoundException"/>, 
-    /// в случае, если полученная коллекция от репозитория будет иметь значение null или пусто.</returns>
+    /// <returns>Колеекция моделей объявлений в сокращенном виде.</returns>
     /// <exception cref="EntitiesNotFoundException"></exception>
     Task<IReadOnlyCollection<AdvertSmallDto>> GetBySearchRequestAsync(AdvertSearchRequestDto request, CancellationToken cancellationToken);
 }
