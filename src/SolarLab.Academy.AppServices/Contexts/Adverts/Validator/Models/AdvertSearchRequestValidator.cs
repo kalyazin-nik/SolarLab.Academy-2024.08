9842139cea2fd78be0b71ed413336f660b341a49
Nikita <kalyazin.nik@yandex.ru>
2024-10-13 11:24:19 +0300

    Поправил правила валидации  для AdvertSearchRequestValidator

--- src/SolarLab.Academy.AppServices/Contexts/Adverts/Validator/Models/AdvertSearchRequestValidator.cs
+++ src/SolarLab.Academy.AppServices/Contexts/Adverts/Validator/Models/AdvertSearchRequestValidator.cs
@@ -8,13 +8,15 @@ public class AdvertSearchRequestValidator : AbstractValidator<AdvertSearchReques
     public AdvertSearchRequestValidator()
     {
         RuleFor(x => x.MinPrice)
+            .GreaterThanOrEqualTo(0).WithMessage("Поле 'MinPrice' должно быть больше или равно нулю.")
             .LessThanOrEqualTo(x => x.MaxPrice).WithMessage("Минимальная цена не должна превышать максимальную.");
 
         RuleFor(x => x.MaxPrice)
-            .GreaterThanOrEqualTo(x => x.MinPrice).WithMessage("Минимальная цена не должна превышать максимальную.");
+            .GreaterThanOrEqualTo(0).WithMessage("Поле 'MaxPrice' должно быть больше или равно нулю.")
+            .GreaterThanOrEqualTo(x => x.MinPrice).WithMessage("Максимальная цена не должна быть меньше минимальной.");
 
         RuleFor(x => x.Take)
-            .NotNull().WithMessage("Поле 'take' обязательно.")
+            .NotNull().WithMessage("Поле 'Take' обязательно.")
             .GreaterThan(0).WithMessage("Поле 'take' должно быть больше нуля.");
     }
 }
